d311c16ea6a0446dfbf45f4891c5665d3ba69a55
Himanshu Rai <swavyast@gmail.com>
2024-01-01 15:02:57 +0530

    tailwind-project-1 : commit - 00000002

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//alert("script is running");
+
+function funct1(){
+    const expr = prompt("Him or Her ?", "Type Here")
+
+    switch(expr){
+        case 'Her':
+            alert("A female")
+            break;
+        case 'Him':
+            alert("A male")
+        case 'None':
+        alert("You haven't entered anything, try again.")
+    }
+}
+
+function funct2(){
+    const promptValue = prompt("What's your age ?")
+    if(promptValue<=0){
+        alert("Age value can't be less than or equal to zero.")
+    }else if(promptValue<18){
+        alert("Minors can't drive")
+    }else{
+        alert("If you know, you can drive else you need to learn how to drive first, then only you can drive.")
+    }
+}
+
+function funct3(){
+
+    const exp = prompt("An expression is either true or false, choose a value and enjoy the magic")
+    // document.write(exp)
+    exp==='true'? alert("You have selected true and since i'm telling it right, it's already magical, isn't it?"): alert("no magic here :/")
+}
